32d1cb525a30319be4987286f3e5393f7340fb39
Vanex <vanessamiglianti@gmail.com>
2019-06-18 18:00:32 +0200

    Aggiunta start activity

---
+++ app/src/main/java/com/example/speech/StartActivity.java
@@ -0,0 +1,27 @@
+package com.example.speech;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+
+                Intent i = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(i);
+
+            }
+        }, 2000);
+
+
+    }
+}
